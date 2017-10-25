04115730e0dafb9f6d360610ecd958012c60e99e
teresa-carmona <31965190+teresa-carmona@users.noreply.github.com>
2017-10-25 15:49:50 +0200

    Initial commit

---
+++ src/main/java/com/lab3/server/lab3server/Lab3ServerApplication.java
@@ -0,0 +1,12 @@
+package com.lab3.server.lab3server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3ServerApplication.class, args);
+	}
+}
